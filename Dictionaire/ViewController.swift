85e0eed1504ffc00f661ca391f1cced4ae91604f
Elisangela Santos <elisangela.sto@icloud.com>
2017-11-14 19:07:29 -0500

    Initial Commit

---
+++ Dictionaire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionaire
+//
+//  Created by Elisangela dos Santos on 17-11-14.
+//  Copyright © 2017 Elisangela dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
