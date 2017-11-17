e03192130de17703b1dccfe53a41bc91669af762
Elisangela Santos <elisangela.sto@icloud.com>
2017-11-17 10:56:37 -0500

    Dernière Version

--- Dictionaire/TableController.swift
+++ Dictionaire/TableController.swift
@@ -5,32 +5,45 @@
 //  Created by Elisangela dos Santos on 17-11-14.
 //  Copyright © 2017 Elisangela dos Santos. All rights reserved.
 //
-
+//============================
 import UIKit
-
+//============================
 class TableController: UIViewController {
     
+    //Variables et connections
     @IBOutlet weak var motFrancais: UITextField!
     @IBOutlet weak var motAnglais: UITextField!
-   
     var arrFrancais: [String]!
     var arrAnglais: [String]!
    
+    //Lorsque le document est prêt
     override func viewDidLoad() {
         super.viewDidLoad()
         managerUser()
-       
     }
     
+    // Ajouter les mots et sauvagarder à memoire
     @IBAction func btnAjouter(_ sender: UIButton) {
         
         arrFrancais.append(motFrancais.text!)
         arrAnglais.append(motAnglais.text!)
         UserDefaults.standard.set(arrFrancais, forKey: "francais")
         UserDefaults.standard.set(arrAnglais, forKey: "anglais")
+        motFrancais.text = ""
+        motAnglais.text = ""
+        let alertController = UIAlertController(title: "Alerte",
+                                                message: "On ajoute de nouveau mot dans le dictionaire!",
+                                                preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Fermer", style:
+        UIAlertActionStyle.cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+        }
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
         
     }
-    
+    //function por verifier les itens dans la memoire
     func managerUser(){
         if UserDefaults.standard.object(forKey: "francais") != nil {
             arrFrancais = UserDefaults.standard.object(forKey: "francais") as! [String]
@@ -40,8 +53,5 @@ class TableController: UIViewController {
             arrAnglais = [String]()
         }
     }
-    
-   
-    
     
 }
--- Dictionaire/ViewController.swift
+++ Dictionaire/ViewController.swift
@@ -5,41 +5,41 @@
 //  Created by Elisangela dos Santos on 17-11-14.
 //  Copyright © 2017 Elisangela dos Santos. All rights reserved.
 //
-
+//=================================
 import UIKit
-
+//=================================
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-   
-   
-    
+
+    // Variables et connections
     @IBOutlet weak var tblView: UITableView!
-    
     @IBOutlet weak var lblresultat: UILabel!
+    @IBOutlet weak var segControl: UISegmentedControl!
     var arrFrancais: [String]!
     var arrAnglais: [String]!
     var DictKeys:[String: String] = [:]
-   
     
-    @IBOutlet weak var segControl: UISegmentedControl!
-    
+   //Lorsque que le document est prêt
     override func viewDidLoad() {
         super.viewDidLoad()
+        tblView.layer.cornerRadius = 5.0
+        segControl.layer.cornerRadius = 5.0
         managerUser()
-        
         DictKeys = Dictionary(uniqueKeysWithValues: zip(arrFrancais, arrAnglais))
+        
     }
-  
+    //Quantite de lignes pour la tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrFrancais.count
     }
     
+    //Ajouter les cell à tableau view
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
-    
+        cell.textLabel?.textColor = UIColor.init(displayP3Red: 28/255, green: 64/255, blue: 142/255, alpha: 1.0)
         cell.textLabel?.text = [String](DictKeys.keys)[indexPath.row]
         return cell
     }
-    
+    //function por verifier les itens dans la memoire
     func managerUser(){
         if UserDefaults.standard.object(forKey: "francais") != nil {
             arrFrancais = UserDefaults.standard.object(forKey: "francais") as! [String]
@@ -50,18 +50,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    // Mettre la traduction de la ligne da table view dans un label
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         lblresultat.text = (Array(DictKeys.values))[indexPath.row]
     }
-   
+    
+    //Métode pour suprimer les mots
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+       
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            arrFrancais.remove(at: indexPath.row)
+            arrAnglais.remove(at: indexPath.row)
+            UserDefaults.standard.set(arrFrancais, forKey: "francais")
+            UserDefaults.standard.set(arrAnglais, forKey: "anglais")
+            refaireDict()
+            tblView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
 
+        }
+    }
+    // Construir le dictionaire
     @IBAction func segAction(_ sender: UISegmentedControl) {
         if segControl.selectedSegmentIndex == 0{
             DictKeys = Dictionary(uniqueKeysWithValues: zip(arrFrancais, arrAnglais))
+            lblresultat.text = ""
         } else {
             DictKeys = Dictionary(uniqueKeysWithValues: zip(arrAnglais, arrFrancais))
+            lblresultat.text = ""
         }
         tblView.reloadData()
+    }
+    
+    //function por refaire le Dictionaire après effacer un mot
+    func refaireDict(){
+        if segControl.selectedSegmentIndex == 0{
+            DictKeys = Dictionary(uniqueKeysWithValues: zip(arrFrancais, arrAnglais))
+            lblresultat.text = ""
+        } else {
+            DictKeys = Dictionary(uniqueKeysWithValues: zip(arrAnglais, arrFrancais))
+            lblresultat.text = ""
+        }
+        
     }
 
 
